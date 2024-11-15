fa3f4f71b83fa9889bf9573451cad18aad6e1c66
syphex97 <marcusswipe@gmail.com>
2024-11-15 17:08:46 +0100

    Function to test viable json strings, updated to match response.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json
-from textwrap import indent
 
 from flask import Flask, render_template, request, jsonify
 
@@ -11,6 +10,15 @@ if __name__ == "__main__":
 
 hash_file_tasks="0"
 
+#Build acceptable json strings
+f_id = {"id": int}
+f_desc = {"description": str}
+f_cat = {"category": str}
+f_stat = {"status": str}
+f_combined = {"id": int, "description": str, "category": str, "status": str}
+f_task_post = {"id": int, "description": str, "category": str}
+variations_list = [f_id, f_desc, f_cat, f_stat, f_combined, f_task_post]
+
 def loadtasks(mode):
     with open("tasks.json", mode) as file:
         data_tasks=json.load(file)
@@ -21,7 +29,7 @@ def get_categories():
     all_categories=[]
     for task in data:
         if task["category"] not in all_categories:
-            all_categories.append(task["category"])
+            all_categories.append((task["category"].lower()).title())
     if len(all_categories)==0:
         return None
     else:
@@ -50,13 +58,27 @@ def html_message_status(message):
     #     return data_tasks
 
 
-
 def notsupported():
     return "Module is not yet supported, its being implemented"
 
-def exists_id(id, list):
-    for i in list:
-        if i["id"]==id: # if user id exists, return True
+
+def correct_json_format(json_string):
+    #Try to load string as python dictionary
+    try:
+        for correct_format in variations_list:
+            if set(json_string.keys())==set(correct_format.keys()):
+                if all(isinstance(json_string[k], v) for k, v in correct_format.items()):
+                    #print(f"The string matched: {correct_format}")
+                    #print(f"json_string is: {json_string}")
+                    return True
+        return False
+    except:
+        print("Invalid json string")
+        return False
+
+def exists_id(id_inpt, list_inpt):
+    for i in list_inpt:
+        if i["id"]==id_inpt: # if user id exists, return True
             return i
 
 # def error_handling(requestform):
@@ -79,24 +101,35 @@ def tasks():
         return loaded_tasks
         #return home()
     if request.method=='POST':
+        if request.form:
+            data=request.form.to_dict()
+        else:
+            data=request.json
         try:
-            int(request.form["id"])
+            int(data["id"])
         except:
             return jsonify({"Error": f"Task id has to be a number!!"})
-        if exists_id(int(request.form["id"]), loaded_tasks):
+        if exists_id(int(data["id"]), loaded_tasks):
             return jsonify({"Error": f"The submited ID already exists, it has to be unique."})
             #return render_template("home.html", title="Todo Website", tasks=loadtasks('r'), show_error=True, show_error_message=f"The task ID already exists: {int(request.form["id"])}")
         else:
+            if not request.form:
+                x=correct_json_format(data)
+                if not x:
+                    x=str(f_task_post).strip(r'\u003Cclass')
+                    return {"Error": f"the input was formated incorrectly, it has to be formed as {x}"}
+            if not data.get("id") or not data.get("description") or not data.get("category"):
+                return {"Error": f"the input was formated incorrectly, it has to be formed as {f_task_post}"}
             loaded_tasks.append({
-                "id": int(request.form["id"]),
-                "description": request.form["description"],
-                "category": request.form["category"],
+                "id": int(data["id"]),
+                "description": data["description"],
+                "category": (data["category"].lower()).title(), #Makes the category input lowercase and then Capitalizes 'Every Word' so categories dont become duplicate.
                 "status": "pending"
                 #"status": request.form["status"]
             })
             with open("tasks.json", 'w') as jfile:
                 json.dump(loaded_tasks, jfile, indent=4)
-                return jsonify({"message": f"Task with id {request.form["id"]} has been added."})
+                return jsonify({"message": f"Task with id {data["id"]} has been added."})
             #return home()
 
 ## Methods GET, DELETE, PUT
@@ -148,7 +181,7 @@ def taskid(task_id):
                 if "description" in data:
                     task["description"]=data["description"]
                 if "category" in data:
-                    task["category"]=data["category"]
+                    task["category"]=(data["category"].lower()).title()
                 if "status" in data:
                     task["status"]=data["status"]
                 with open("tasks.json", 'w') as jfile:
--- main.py
+++ main.py
@@ -1,1 +1,57 @@
+from sys import prefix
+
 print("This does nothing, this is just here for existing")
+
+# Testing code for website
+
+# Capitalises first letter for every word in the json file
+# ftasks=loadtasks('r')
+# for task in ftasks:
+#     task["category"]=task["category"].title()
+#     with open("tasks.json", 'w') as jfile:
+#         json.dump(ftasks, jfile, indent=4)
+
+
+
+def correct_json_format(json_string):
+    f_id = {"id": int}
+    f_desc = {"description": str}
+    f_cat = {"category": str}
+    f_stat = {"status": str}
+    f_combined = {"id": int, "description": str, "category": str, "status": str}
+    variations_list = [f_id, f_desc, f_cat, f_stat, f_combined]
+
+    #Try to load string as python dictionary
+    try:
+        for correct_format in variations_list:
+            if set(json_string.keys())==set(correct_format.keys()):
+                if all(isinstance(json_string[k], v) for k, v in correct_format.items()):
+                    #print(f"The string matched: {correct_format}")
+                    #print(f"json_string is: {json_string}")
+                    return True
+        return False
+    except:
+        print("Invalid json string")
+        return False
+
+id_int = {"id": 1}
+id_text = {"id": "abc"}
+id_comb =     {
+        "id": 123,
+        "description": "Buy milk, eggs and bread",
+        "category": "Shopping"
+    }
+
+print("Testing id_int")
+correct_json_format(id_int)
+print("....1....")
+print("Testing id_str")
+print(correct_json_format(id_text))
+print("....2....")
+print("testing whole formated line now")
+print(correct_json_format(id_comb))
+
+print("############")
+id_text_invalid = "id"
+print("Testing invalid lines now")
+correct_json_format(id_text_invalid)
